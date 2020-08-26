3c4b946c03ed7da676ecabc74f8ff167c3c1a70b
harshthakur066 <harshthakur066@gmail.com>
2020-08-26 23:25:39 +0530

    showing current location on map

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:place_app/helpers/location_helper.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -7,6 +9,37 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+
+  Location location = new Location();
+
+  Future<void> _getUserCurrentLocation() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    final locData = await location.getLocation();
+    final staticMapImageUrl = LocationHelper.generateLocationPreviewImage(
+      longitude: locData.longitude,
+      latitude: locData.latitude,
+    );
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +67,7 @@ class _LocationInputState extends State<LocationInput> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             FlatButton.icon(
-              onPressed: () {},
+              onPressed: _getUserCurrentLocation,
               icon: Icon(Icons.location_on),
               label: Text('Current Location'),
               textColor: Theme.of(context).primaryColor,
